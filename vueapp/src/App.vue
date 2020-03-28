89b52c3247aea32c79e294d1ab32b46ae2b86dd5
Nina Voelker <Nina@Ninas-MBP-4.fritz.box>
2020-03-28 14:17:20 +0100

    commit

---
+++ vueapp/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <CentralButton />
+  </div>
+</template>
+
+<script>
+import CentralButton from "./components/CentralButton.vue";
+
+export default {
+  name: "App",
+  components: {
+    CentralButton
+  }
+};
+</script>
+
+<style lang="scss">
+@import "components/sass/_variables";
+@import "components/sass/_mixins";
+
+body {
+  margin: 0;
+  height: 100vh;
+
+  #app {
+    text-align: center;
+    background-color: $eerie-black;
+    color: $azureish-white;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
+  }
+}
+</style>
---
+++ vueapp/src/components/CentralButton.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <button>Button</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CentralButton",
+  props: {}
+};
+</script>
+
+<style lang="scss" scoped>
+@import "./sass/_variables";
+@import "./sass/_mixins";
+
+@import url('https://fonts.googleapis.com/css?family=Monoton&display=swap');
+
+button {
+  @include button;
+  @include button-shadow;
+  color: $azureish-white;
+  font-size: 1em;
+  margin: 40px;
+  font-size: 3em;
+  text-shadow: 2px 2px 5px $azureish-white;
+  border: 3px dotted $azureish-white;
+  font-family: 'Monoton', cursive;
+
+  &:hover {
+    background: $pumpkin-gradient;
+    @include button-active;
+    border: none;
+      text-shadow: 2px 2px 5px $eerie-black;
+
+  }
+}
+</style>
